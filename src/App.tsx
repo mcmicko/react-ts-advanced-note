b320f350d2e454ea6dd15638266e270d4448e5de
mcmicko <mcmzrc@gmail.com>
2022-11-24 19:12:39 +0100

    delete note

--- src/App.tsx
+++ src/App.tsx
@@ -69,6 +69,12 @@ function App() {
     });
   }
 
+  function onDeleteNote(id: string) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -90,7 +96,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,15 @@
 import { Badge, Button, Col, Row, Stack } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useNote } from './NoteLayout';
 import ReactMarkdown from 'react-markdown';
 
-export default function Note() {
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
+
+export default function Note({ onDelete }: NoteProps) {
   const note = useNote();
+  const navigate = useNavigate();
   return (
     <>
       <Row className="align-items-center mb-4">
@@ -27,7 +32,15 @@ export default function Note() {
               <Button variant="primary">Edit</Button>
             </Link>
             <Link to={`/`}>
-              <Button variant="outline-danger">Delete</Button>
+              <Button
+                onClick={() => {
+                  onDelete(note.id);
+                  navigate('/');
+                }}
+                variant="outline-danger"
+              >
+                Delete
+              </Button>
             </Link>
 
             <Button variant="outline-secondary">Back</Button>
